aade54844ce356ccde722b76a3eaa1f905c6e7c0
Alex <alex0popa@gmail.com>
2024-12-07 13:16:55 +0100

    r day 5

--- solutions/year_2024/day_5.ts
+++ solutions/year_2024/day_5.ts
@@ -2,76 +2,35 @@ import { extractNumbers } from '../../helpers/general';
 import { getInputForDay } from '../../helpers/getInputForDay';
 import { showTheResult } from '../../helpers/showTheResult';
 
-const isOrdered = (rules: string[], pageNumbers: number[]) => {
+const isOrdered = (rulesSet: Set<string>, pageNumbers: number[]) => {
+  let matchedRules = 0;
+
   for (let i = 1; i < pageNumbers.length; ++i) {
-    if (!rules.includes(`${pageNumbers[i - 1]}|${pageNumbers[i]}`)) {
-      return false;
+    if (rulesSet.has(`${pageNumbers[i - 1]}|${pageNumbers[i]}`)) {
+      matchedRules++;
     }
   }
-  return true;
+
+  return matchedRules === pageNumbers.length - 1;
 };
 
-/**
- * There is always a number without pair, so it go to the last place.
- * All others have pairs, bur all have a different number of pairs.
- *
- * Ordering by number of pairs we see that every lastNumber has only a match on the next array of pairs.
- * 
- * Use this log to visualize:
- * 
- * console.log('\n', pageNumbers.join(', '), '\n');
- * Object.entries(map)
- *  .sort((a, b) => a[1].length - b[1].length)
- * .forEach(([pageNumber, nums]) => {
- *    console.log(`${pageNumber} => ${nums.join(', ')}`);
- *  });
-  console.log('\n###################################\n');
- *
- * However, we can see that the target number is in the middle array.  
- */
-const getMiddlePageFromUnordered = (rules: string[], pageNumbers: number[]) => {
-  const map: { [key: number]: string[] } = {};
-  let lastNumber = 0;
-
-  for (let i = 0; i < pageNumbers.length; ++i) {
-    const pageNumber = pageNumbers[i];
-    map[pageNumber] = [];
+const getMiddlePageFromUnordered = (
+  rulesSet: Set<string>,
+  pageNumbers: number[],
+) => {
+  for (const pageNumber of pageNumbers) {
+    let matchedRules = 0;
 
     for (const num of pageNumbers) {
-      if (num !== pageNumber) {
-        if (rules.includes(`${pageNumber}|${num}`)) {
-          map[pageNumber].push(`${pageNumber}|${num}`);
-        }
+      if (num !== pageNumber && rulesSet.has(`${pageNumber}|${num}`)) {
+        matchedRules++;
       }
     }
 
-    // last number is always without pair
-    if (map[pageNumber].length === 0) {
-      delete map[pageNumber];
-      lastNumber = pageNumber;
-    }
+    if (matchedRules === (pageNumbers.length - 1) / 2) return pageNumber;
   }
 
-  const orderedPageNumbers = Object.entries(map)
-    // extract pairs
-    .map(([, pairs]) => pairs)
-    // sort by number of pairs
-    .sort((pair1, pair2) => pair1.length - pair2.length)
-    // get the first half, at the half we have the target number
-    .slice(0, (pageNumbers.length - 1) / 2);
-
-  for (const pairs of orderedPageNumbers) {
-    for (const pair of pairs) {
-      const [front, back] = pair.split('|').map(Number);
-
-      if (back === lastNumber) {
-        lastNumber = front;
-        break;
-      }
-    }
-  }
-
-  return lastNumber;
+  return 0;
 };
 
 (async () => {
@@ -79,6 +38,7 @@ const getMiddlePageFromUnordered = (rules: string[], pageNumbers: number[]) => {
   const input = (await getInputForDay(__filename)).trim().split('\n');
   const emptyLineIdx = input.findIndex((line) => line.length < 5);
   const rules = input.slice(0, emptyLineIdx);
+  const rulesSet = new Set(rules);
   const pages = input.slice(emptyLineIdx + 1);
   let star1 = 0;
   let star2 = 0;
@@ -86,13 +46,13 @@ const getMiddlePageFromUnordered = (rules: string[], pageNumbers: number[]) => {
   for (const line of pages) {
     const pageNumbers = extractNumbers(line);
 
-    if (isOrdered(rules, pageNumbers)) {
+    if (isOrdered(rulesSet, pageNumbers)) {
       star1 += pageNumbers[(pageNumbers.length - 1) / 2];
 
       continue;
     }
 
-    star2 += getMiddlePageFromUnordered(rules, pageNumbers);
+    star2 += getMiddlePageFromUnordered(rulesSet, pageNumbers);
   }
 
   showTheResult({ star1, star2, path: __filename });
